d3246d7b63a9443d5ccc2a3f4057814faa62cb10
Eric Demauro <erickdemauro@gmail.com>
2016-12-23 15:24:21 -0800

    initial commit.

---
+++ app/src/main/java/com/example/ericdemauro/geoquiz/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ericdemauro.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+    }
+}
